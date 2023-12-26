dc46456a578e43a856d5262a31b90ee5d280d960
dominika232323 <dominikaboguszewska62@gmail.com>
2023-12-26 18:03:52 +0100

    Start implementing TwoLayerPerceptron

--- two_layer_perceptron.py
+++ two_layer_perceptron.py
@@ -1,3 +1,28 @@
+import numpy as np
+
+
 class TwoLayerPerceptron:
-    def __init__(self, hidden):
+    def __init__(self, hidden, learning_rate):
         self._hidden = hidden
+        self._learning_rate = learning_rate
+
+        self._hidden_layer_weights = np.random.uniform((-1), 1, size=(hidden, 2))
+        self._output_layer_weights = np.random.uniform((-1), 1, size=(1, hidden + 1))
+
+    @property
+    def hidden_size(self):
+        return self._hidden
+
+    @property
+    def learning_rate(self):
+        return self._learning_rate
+
+    def train(self, dataset, epochs):
+        for epoch in epochs:
+            pass
+
+    def _activation_function(self, x):
+        return np.tanh(x)
+
+    def predict(self):
+        pass
