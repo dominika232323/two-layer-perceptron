392fe206f636f02bd84c84c23f5db86489264c49
dominika232323 <dominikaboguszewska62@gmail.com>
2023-12-26 17:50:57 +0100

    Add create_points_for_function

--- main.py
+++ main.py
@@ -8,5 +8,16 @@ def approximated_function(x):
     return np.sin(x * np.sqrt(Constants.p0() + 1)) + np.cos(x * np.sqrt(Constants.p1() + 1))
 
 
+def create_points_for_function(function, lower_bound, upper_bound, how_many):
+    points = []
+    x_coords = np.linspace(lower_bound, upper_bound, how_many)
+
+    for x in x_coords:
+        y = function(x)
+        points.append([x, y])
+
+    return points
+
+
 if __name__ == '__main__':
     draw_approximated_function_approximation_plot(Constants.lower_bound(), Constants.upper_bound(), approximated_function)
